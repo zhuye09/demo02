a17f2c5aeb68a898593d314e31bdb70ad52edb68
zhuye09 <ye.zhu@hand-china.com>
2017-08-12 13:01:51 +0800

    new

--- Test/src/com/hand/one/Sal.java
+++ Test/src/com/hand/one/Sal.java
@@ -15,17 +15,17 @@ public static void main(String[] args) {
 		if(i<1500){
 			money = (i * 0.03);
 		}else if(i>1500 && i<4500){
-			money = i * 0.1;
+			money = (i-1500) * 0.1;
 		}else if(i>4500 && i<9000){
-			money = i * 0.2;
+			money = (i-4500) * 0.2;
 		}else if(i>9000 && i<35000){
-			money = i * 0.25;
-		}else if(i>3500 && i<55000){
-			money = i * 0.3;
-		}else if(i>5500 && i<80000){
-			money = i * 0.35;
+			money = (i-9000) * 0.25;
+		}else if(i>35000 && i<55000){
+			money = (i-35000) * 0.3;
+		}else if(i>55000 && i<80000){
+			money = (i-55000) * 0.35;
 		}else if(i>80000 ){
-			money = i * 0.45;
+			money = (i-80000) * 0.45;
 		}
 		System.out.println("所需要缴纳的税费为："+money);
 	} catch (Exception e) {
